b32ec0177aaa516d3163398883aab6ca2afcc1f1
Marcos Fabian <you@example.com>
2020-07-30 23:54:35 +0800

    Added a categories to the left, links not working

--- news-app/src/App.vue
+++ news-app/src/App.vue
@@ -1,6 +1,39 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app>
+      <div class="left-grid">
+        <h2>Categories: </h2>
+        <a href="#">Health</a>
+        <a href="#">Finances</a>
+        <a href="#">Business</a>
+        <a href="#">Tech</a>
+        <a href="#">Sports</a>
+        <a href="#">Weather</a>
+        <a href="#">Economy</a>
+      </div>
+      <div class="left-grid">
+        <h2>Country: </h2>
+        <a href="#">United Kingdom</a>
+        <a href="#">United States</a>
+        <a href="#">Spain</a>
+        <a href="#">Greece</a>
+        <a href="#">Rusia</a>
+        <a href="#">France</a>
+        <a href="#">Italy</a>
+        <a href="#">Germany</a>
+      </div>
+      <div class="left-grid">
+        <h2>Language: </h2>
+        <a href="#">English</a>
+        <a href="#">Spanish</a>
+        <a href="#">Italian</a>
+        <a href="#">Russian</a>
+        <a href="#">French</a>
+        <a href="#">Japanese</a>
+        <a href="#">Chinese</a>
+        <a href="#">German</a>
+      </div>
+    </v-navigation-drawer>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <v-btn to="/" flat>
@@ -30,4 +63,13 @@ export default {
     }
   }
 }
+
 </script>
+
+<style scoped>
+  .left-grid {
+    text-align: center;
+    display: grid;
+    margin: 15px;
+  }
+</style>
